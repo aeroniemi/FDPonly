9978cf9fcdf8926bf55a7ce4e030f2d52be38058
aeroniemi <flightcontrol222@gmail.com>
2017-12-17 17:39:02 +0000

    varous changes

--- index.js
+++ index.js
@@ -2,78 +2,79 @@ var config = require('./config.js');
 var readline = require('readline');
 var Datastore = require('nedb');
 var db = new Datastore({
-	filename: 'airports.database',
-	autoload: true
+    filename: 'airports.database',
+    autoload: true
 });
 var datas = {
-	airport: {
-		icao: "EGKK",
-		type: "commerical",
-		ele: "154",
-		variation: "3.05",
-		iata: "LGW",
-		city: "London",
-		name: "Gatwick Airport",
-		lat: "0",
-		lon: "0",
-		runways: {
-			"1": {
-				"end": [{
-						"name": "08R",
-						"heading": "085",
-						"length": "2440m",
-						"type": "ice"
+    airport: {
+        icao: "EGLL",
+        type: "commerical",
+        ele: "124",
+        variation: "7.05",
+        iata: "LHR",
+        city: "London",
+        name: "Heathrow Airport",
+        lat: "0",
+        lon: "0",
+        runways: {
+            "1": {
+                "end": [{
+                        "name": "08R",
+                        "heading": "085",
+                        "length": "2440m",
+                        "type": "ice"
 				},
-					{
-						"name": "26L",
-						"heading": "265",
-						"length": "2440m",
-						"type": "ice"
+                    {
+                        "name": "26L",
+                        "heading": "265",
+                        "length": "2440m",
+                        "type": "ice"
 				}]
-			},
-			"2": {
-				"end": [{
-						"name": "08L",
-						"heading": "085",
-						"length": "2440m",
-						"type": "ice"
+            },
+            "2": {
+                "end": [{
+                        "name": "08L",
+                        "heading": "085",
+                        "length": "2440m",
+                        "type": "ice"
 				},
-					{
-						"name": "26R",
-						"heading": "265",
-						"length": "2440m",
-						"type": "ice"
+                    {
+                        "name": "26R",
+                        "heading": "265",
+                        "length": "2440m",
+                        "type": "ice"
 				}]
-			},
-		}
-	}
+            },
+        }
+    }
 
 
 };
 db.insert(datas, function (err, newDoc) { // Callback is optional
-	// newDoc is the newly inserted document, including its _id
-	// newDoc has no key called notToBeSaved since its value was undefined
+    // newDoc is the newly inserted document, including its _id
+    // newDoc has no key called notToBeSaved since its value was undefined
 });
+/*
 var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 var data = {}
 rl.question("Airport ICAO: ", function (icao) {
-	data[icao] = {}
-	console.log(data)
-	rl.question("type: ", function (answer1) {
-		data[icao].type = answer1
+    data[icao] = {}
+    console.log(data)
+    rl.question("type: ", function (answer1) {
+        data[icao].type = answer1
 
-		rl.question("elevation: ", function (answer2) {
-			data[icao].elevation = answer2
+        rl.question("elevation: ", function (answer2) {
+            data[icao].elevation = answer2
 
-			rl.question("variation: ", function (answer1) {
-				data[icao].variation = answer1
+            rl.question("variation: ", function (answer1) {
+                data[icao].variation = answer1
 
-				console.log(data)
-				rl.close();
-			});
-		});
-	});
-});
+                console.log(data)
+                rl.close();
+            });
+        });
+    });
+});*/
--- routes.js
+++ routes.js
@@ -1,119 +1,124 @@
 module.exports = function (app, config, db) {
-	var datas = {
-		"airports": {
-			"EGKK": {
-				"type": "commerical",
-				"ele": "154",
-				"variation": "3.05",
-				"icao": "EGKK",
-				"iata": "LGW",
-				"city": "London",
-				"name": "Gatwick Airport",
-				"lat": "0",
-				"lon": "0",
-				"runways": {
-					"1": {
-						"end": [{
-								"name": "08R",
-								"heading": "085",
-								"length": "2440m",
-								"type": "ice"
+    var datas = {
+        "airports": {
+            "EGKK": {
+                "type": "commerical",
+                "ele": "154",
+                "variation": "3.05",
+                "icao": "EGKK",
+                "iata": "LGW",
+                "city": "London",
+                "name": "Gatwick Airport",
+                "lat": "0",
+                "lon": "0",
+                "runways": {
+                    "1": {
+                        "end": [{
+                                "name": "08R",
+                                "heading": "085",
+                                "length": "2440m",
+                                "type": "ice"
 				},
-							{
-								"name": "26L",
-								"heading": "265",
-								"length": "2440m",
-								"type": "ice"
+                            {
+                                "name": "26L",
+                                "heading": "265",
+                                "length": "2440m",
+                                "type": "ice"
 				}]
-					},
-					"2": {
-						"end": [{
-								"name": "08L",
-								"heading": "085",
-								"length": "2440m",
-								"type": "ice"
+                    },
+                    "2": {
+                        "end": [{
+                                "name": "08L",
+                                "heading": "085",
+                                "length": "2440m",
+                                "type": "ice"
 				},
-							{
-								"name": "26R",
-								"heading": "265",
-								"length": "2440m",
-								"type": "ice"
+                            {
+                                "name": "26R",
+                                "heading": "265",
+                                "length": "2440m",
+                                "type": "ice"
 				}]
-					},
-				}
-			}
-		}
-
-
-	};
-	if (config.website.https == true) {
-		var rootaddressMerge = "https://" + config.website.rootAddress
-	} else {
-		var rootaddressMerge = "http://" + config.website.rootAddress
-	}
-
-	app.get('/:icao/atc', function (req, res) {
-		var icao = req.params.icao.toUpperCase();
-		//console.log(icao)
-
-		var merged = {
-			"airport": datas.airports[icao],
-			"atc": true,
-			"rootAddress": rootaddressMerge
-		}
-		console.log(merged)
-		res.render('airport', merged)
-	});
-	app.get('/:icao/pilot', function (req, res) {
-		var icao = req.params.icao.toUpperCase();
-		//console.log(icao)
-		var merged = {
-			"airport": datas.airports[icao],
-			"atc": false,
-			"rootAddress": rootaddressMerge
-		}
-		console.log(merged)
-		res.render('airport', merged)
-	});
-
-	app.get('/:icao', function (req, res) {
-		var icao = req.params.icao.toUpperCase();
-		//console.log(icao)
-		/*
-        var merged = {
-            "airport": datas.airports[icao],
-            "atc": false,
-            "rootAddress": rootaddressMerge
+                    },
+                }
+            }
         }
-        console.log(merged)
-        res.render('airport', merged)
-*/
-		db.findOne({
-			"airport.icao": icao
-		}, function (err, docs) {
-			//console.log(docs)
-			if (docs != "[]") {
-				var merged = {
-					"airport": docs.airport,
-					"atc": false,
-					"rootAddress": rootaddressMerge
-				}
-			} else {
-				var merged = {
-					"error": err,
-					"atc": false,
-					"rootAddress": rootaddressMerge
-				}
+    };
+    if (config.website.https == true) {
+        var rootaddressMerge = "https://" + config.website.rootAddress
+    } else {
+        var rootaddressMerge = "http://" + config.website.rootAddress
+    }
+    app.get('/:icao/atc', function (req, res) {
+        var icao = req.params.icao.toUpperCase();
+        db.findOne({
+            "airport.icao": icao
+        }, function (err, docs) {
+            //console.log(docs)
+            if (docs != "[]") {
+                var merged = {
+                    "airport": docs.airport,
+                    "atc": true,
+                    "rootAddress": rootaddressMerge
+                }
+            } else {
+                var merged = {
+                    "error": err,
+                    "atc": true,
+                    "rootAddress": rootaddressMerge
+                }
+            }
+            res.render('airport', merged)
+        });
+    });
+    app.get('/:icao/pilot', function (req, res) {
+        var icao = req.params.icao.toUpperCase();
+        db.findOne({
+            "airport.icao": icao
+        }, function (err, docs) {
+            //console.log(docs)
+            if (docs != "[]") {
+                var merged = {
+                    "airport": docs.airport,
+                    "atc": false,
+                    "rootAddress": rootaddressMerge
+                }
+            } else {
+                var merged = {
+                    "error": err,
+                    "atc": false,
+                    "rootAddress": rootaddressMerge
+                }
+            }
+            res.render('airport', merged)
+        });
+    });
 
-			}
-			//console.log(merged);
-			res.render('airport', merged)
-		});
+    app.get('/:icao', function (req, res) {
+        var icao = req.params.icao.toUpperCase();
+        db.findOne({
+            "airport.icao": icao
+        }, function (err, docs) {
+            //console.log(docs)
+            if (docs != "[]") {
+                var merged = {
+                    "airport": docs.airport,
+                    "atc": false,
+                    "rootAddress": rootaddressMerge
+                }
+            } else {
+                var merged = {
+                    "error": err,
+                    "atc": false,
+                    "rootAddress": rootaddressMerge
+                }
+            }
+            res.render('airport', merged)
+        });
+    });
 
-	});
-
-	app.get('/', function (req, res) {
-		res.render('home');
-	});
+    app.get('/', function (req, res) {
+        res.render('home');
+    });
 
 };
