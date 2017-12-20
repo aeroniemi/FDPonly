9a37659bbf863ae46d0ffbd744a257d191f03c95
Alex Beavil <flightcontrol222@gmail.com>
2017-12-20 14:21:46 +0000

    promises?

--- routes.js
+++ routes.js
@@ -50,44 +50,7 @@ module.exports = function (app, config, db) {
             }
         }
     };
-    //promises
 
-    var metarPrCall = new Promise(
-        function (resolve, reject) {
-            metarFetcher.getData(icao).then(function (response) {
-                console.log(response)
-                resolve(response);
-            }, function (error) {
-                console.error(error);
-                reject(error);
-            });
-        }
-    );
-    var tafPrCall = new Promise(
-        function (resolve, reject) {
-            tafFetcher.getData(icao).then(function (response) {
-                console.log(response)
-                resolve(response);
-            }, function (error) {
-                console.error(error);
-                reject(error);
-            })
-        }
-    );
-
-    var notamPrCall = new Promise(
-        function (resolve, reject) {
-            if (icao.charAt(0) == 'K' || icao.charAt(0) == 'P') {
-                resolve(require('notams')(icao, {
-                    format: 'DOMESTIC'
-                }))
-            } else {
-                resolve(require('notams')(icao, {
-                    format: 'ICAO'
-                }))
-            }
-        }
-    );
     // web address merger
     if (config.website.https == true) {
         var rootaddressMerge = "https://" + config.website.rootAddress
@@ -98,35 +61,56 @@ module.exports = function (app, config, db) {
     // atc page compiler
     app.get('/:icao/atc', function (req, res) {
         var icao = req.params.icao.toUpperCase();
-        var weatherIcao = require('./compilePageData.js')(icao);
-        console.log(weatherIcao.mergedWeather);
-        db.findOne({
-            "airport.icao": icao
-        }, function (err, docs) {
-            //console.log(docs)
-            if (docs != "[]") {
-                var merged = {
-                    "airport": docs.airport,
-                    "weather": weatherIcao.weather,
-                    "atc": true,
-                    "rootAddress": rootaddressMerge,
+        var dbPrCall = new Promise(
+            function (resolve, reject) {
+                db.findOne({
+                    "airport.icao": icao
+                }, function (err, docs) {
+                    //console.log(docs)
+                    if (docs != "[]") {
+                        resolve(docs);
+                    } else {
+                        reject(err)
+                    }
+                    //console.log(merged);
+
                 }
-            } else {
-                var merged = {
-                    "error": err,
-                    "atc": true,
-                    "rootAddress": rootaddressMerge
-                }
+                )
             }
-            //console.log(merged);
+        );
+        var metarPrCall = new Promise(
+            function (resolve, reject) {
+                metarFetcher.getData(icao).then(function (response) {
+                    //console.log(response)
+                    resolve(response);
+                }, function (error) {
+                    console.error(error);
+                    reject(error);
+                });
+            }
+        );
+        var tafPrCall = new Promise(
+            function (resolve, reject) {
+                tafFetcher.getData(icao).then(function (response) {
+                    //console.log(response)
+                    resolve(response);
+                }, function (error) {
+                    console.error(error);
+                    reject(error);
+                })
+            }
+        );
+        Promise.all(metarPrCall, tafPrCall, dbPrCall).then(values => {
+            var merged = {
+                "weather": {
+                    "metar": values[1],
+                    "taf": values[2]
+                },
+                "airport": values.airport,
+                "atc": true,
+                "rootAddress": rootaddressMerge
+            }
             res.render('airport', merged)
-        });
-        Promise.all([notamPrCall, metarPrCall, tafPrCall]).then(values => {
-            merged = + {
-                "metar": values[1],
-                "taf": values[2],
-                "notams": values[0][0].notams
-            }
             console.log(merged)
         });
     })
