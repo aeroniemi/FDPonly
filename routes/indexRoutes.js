3da25fb8e887be12dcc738c42461b6b93a61d805
aeroniemi <flightcontrol222@gmail.com>
2017-12-29 21:25:13 +0000

    various fixes to problems that didn't exist

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('home');
+    res.render('indexZX');
 });
+/*
+router.get('/:icao', function (req, res, next) {
+    var icao = req.params.id.toUpperCase();
+    res.redirect('airports/' + icao);
+});
+*/
 module.exports = router;
